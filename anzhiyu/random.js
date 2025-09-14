33d60be2cb9cb38ac4e52ccc0b9b155a8740d144
quyongxuan1 <qyx20080822@gmail.com>
2025-09-14 08:57:51 +0800

    Site updated: 2025-09-14 08:57:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/04/hello-world/"];function toRandomPost(){
+var posts=["2025/09/04/hello-world/","2025/09/14/节点/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
